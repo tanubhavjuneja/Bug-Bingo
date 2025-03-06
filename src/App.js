16e45683275b87535f56659bd7cb850cec0dae7e
tanubhavjuneja <tanubhavj@gmail.com>
2025-03-06 16:44:18 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Registration from "./Registration";
 import BugBingo from "./Game";
+import Rules from "./Rules";
 import BugBingo1 from "./Game1";
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Registration />} />
+        <Route path="/" element={<Rules />} />
+        <Route path="/registration" element={<Registration />} />
         <Route path="/game" element={<BugBingo />} />
         <Route path="/game_final" element={<BugBingo1 />} />
         </Routes>
